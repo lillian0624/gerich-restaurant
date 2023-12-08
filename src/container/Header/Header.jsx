94cbe78b25558b6b42cdeeae5cbdfe718ec261e0
Lillian Yang <nopeyangli@163.com>
2023-12-08 13:06:07 +1100

    add padding to header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,7 +5,7 @@ import { SubHeading } from '../../components';
 import {images} from "../../constants";
 
 const Header = () => (
-  <div className='app__header app__wrapper'>
+  <div className='app__header app__wrapper section__padding' id="home">
     <div className='app__wrapper_info'>
       <SubHeading title="Chase the new flavour"/>
       <h1 className='app__header-h1'>The Key to Fine Dining</h1>
