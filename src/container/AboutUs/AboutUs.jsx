c444005706a0110c438144801e588cd158642218
Lillian Yang <nopeyangli@163.com>
2023-12-12 12:12:26 +1100

    laurels

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -4,7 +4,7 @@ import './AboutUs.css';
 import { images } from '../../constants';
 
 const AboutUs = () => (
-  <div className='app__aboutus app__bg flex__center section__padding id="about'>
+  <div className='app__aboutus app__bg flex__center section__padding id="about' id='about'>
     <div className='app__aboutus-overlay flex__center'>
       <img src={images.G} alt="g letter" />
       </div>
